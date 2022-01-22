e5ec91bb6f44ac8603ff7ecb0aca05191cf0583a
Michel Marcondes <michel.marcondes@gmail.com>
2022-01-22 22:32:41 +0000

    Passing data to templates and dynamic urls

--- app/projects/views.py
+++ app/projects/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        'id':'1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio'
+    },
+    {
+        'id':'3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working on'
+    },
+]
+
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello, you are on the projects page'
+    context = {'message': msg, 'projects': projectList}
+
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-     return render(request, 'projects/single-project.html')
+    projectObj = None
+
+    for project in projectList:
+        if project['id'] == pk:
+            projectObj = project
+    
+    return render(request, 'projects/single-project.html', {'project': projectObj})

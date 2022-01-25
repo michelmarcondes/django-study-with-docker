473ddad527c0ac16032ffd526d8c73d46cf48714
Michel Marcondes <michel.marcondes@gmail.com>
2022-01-25 21:19:04 +0000

    revoke secret key and using environment variables

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i3$fj9g(_auq2b*br(uv681ic7&wj$h*wd%6sli&mdprgd=3vx'
+#SECRET_KEY = 'django-insecure-i3$fj9g(_auq2b*br(uv681ic7&wj$h*wd%6sli&mdprgd=3vx'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

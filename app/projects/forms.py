49e6b6080f36fa1b3d35d8db725376ead547ee4e
Michel Marcondes <michel.marcondes@gmail.com>
2022-01-26 00:21:27 +0000

    styling project with a theme

--- app/projects/forms.py
+++ app/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -7,3 +8,17 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
         #fields = '__all__'
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    #override class constructor
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        #customizing field manual method
+        # self.fields['title'].widget.attrs.update({'class':'input'})
+
+        #customizing field dinamically
+        for name, field in self.fields.items():
+            field.widget.attrs. update({'class':'input'})

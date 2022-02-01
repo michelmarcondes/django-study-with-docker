8fbd7e1fd3842173dfbce97b73da76465c4ed7a7
Michel Marcondes <michel.marcondes@gmail.com>
2022-02-01 01:38:31 +0000

    Styling Login & Register Page

--- app/static/uikit/app.js
+++ app/static/uikit/app.js
@@ -1,4 +1,17 @@
 // Invoke Functions Call on Document Loaded
 document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
+  // hljs.highlightAll();
 });
+
+let alertWrapper = document.querySelector('.alert')
+let alertClose = document.querySelector('.alert__close')
+
+
+if (alertWrapper) {
+  // console.log('alert wrapper found')
+  alertClose.addEventListener('click', () => {
+    // console.log('alert close clicked')
+    alertWrapper.style.display = 'none'
+    }
+  )
+}
--- app/users/forms.py
+++ app/users/forms.py
@@ -1,3 +1,4 @@
+from operator import contains
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,3 +11,14 @@ class CustomUserCreationForm(UserCreationForm):
         labels = {
             'first_name': 'Name'
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        print(self.fields.items())
+
+        for name, field in self.fields.items():
+            if str(name).find('password') > -1:
+                field.widget.attrs.update({'placeholder': '••••••••'})
+
+            field.widget.attrs.update({'class': 'input input--text'})

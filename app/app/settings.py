cb77b01cb4fb9f0150455c94aeb221859ae56fd7
Michel Marcondes <michel.marcondes@gmail.com>
2022-02-13 01:06:04 +0000

    Sending email from app

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,6 +123,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Send Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
--- app/users/signals.py
+++ app/users/signals.py
@@ -1,47 +1,66 @@
-#SIGNALS
-import profile
+# SIGNALS
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
 from .models import Profile
 
+# for email use
+from django.core.mail import send_mail
+from django.conf import settings
 
 
-#using decorators to signals
+# using decorators to signals
 
-#Automatically create a Profile when a user is created
+# Automatically create a Profile when a user is created
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
         profile = Profile.objects.create(
-            user = user,
-            username = user.username,
-            email = user.email,
-            name = user.first_name,
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
         )
 
-#update user credentials when a profile was updated
+        # send welcome email
+        subject = 'Welcome to DevSearch Django Project'
+        message = '''Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. 
+                    To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words.
+
+                    If several languages coalesce, the grammar of the resulting language is more simple and regular than 
+                    that of the individual languages. The new common language will be more simple and regular than the 
+                    existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.'''
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+
+# update user credentials when a profile was updated
 @receiver(post_save, sender=Profile)
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
     user = profile.user
-    if created == False: #check if user doesnt exist
+    if created == False:  # check if user doesnt exist
         user.first_name = profile.name
         user.username = profile.username
         user.email = profile.email
         user.save()
-    
 
 
-#When a profile is deleted, user also is deleted
-#to avoid orphans data
+# When a profile is deleted, user also is deleted
+# to avoid orphans data
 @receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
-    
+
 # Connecting signals without using decorators
 # post_save.connect(profileUpdated, sender=Profile)
 # post_delete.connect(deleteUser, sender=Profile)

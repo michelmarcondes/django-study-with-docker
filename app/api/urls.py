f5c8e344e1b3e15528235e4c87df77bb9b722846
Michel Marcondes <michel.marcondes@gmail.com>
2022-02-17 00:24:26 +0000

    Using JWT authentication in REST API

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
     path('', views.getRoutes),
 
     path('projects/', views.getProjects),
     path('projects/<str:pk>/', views.getProject),
+
+    path('projects/<str:pk>/vote/', views.projectVote),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,8 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response 
 
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
@@ -19,6 +20,7 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True) # set object to serialize and define 'many' to retrive more than a single record
@@ -26,7 +28,33 @@ def getProjects(request):
 
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner = profile,
+        project = project,
+    )
+
+    review.value = data['vote']
+    review.save()
+
+    print(review)
+
+    # update vote count
+    project.getVoteCount
+
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
